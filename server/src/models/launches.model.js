7dc59599f7f36b0a4e24324c3757df5b4d1e2b33
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-11-02 23:17:07 +0200

    Update

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,5 +1,7 @@
 const launches = new Map();
 
+let latestFlightNumber = 100;
+
 const launch = {
     flightNumber: 100,
     mission: 'Kepler Exploration X',
@@ -17,4 +19,17 @@ function getAllLaunches(){
     return Array.from(launches.values());
 }
 
-module.exports = { getAllLaunches };
+function addNewLaunch(launch){
+    latestFlightNumber++;
+    launches.set(
+      latestFlightNumber,
+      Object.assign(launch, {
+        success: true,
+        upcoming: true,
+        customer: ["Zero to master", "NASA"],
+        flightNumber: latestFlightNumber,
+      })
+    );
+}
+
+module.exports = { getAllLaunches, addNewLaunch };
--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -1,5 +1,9 @@
-const { getAllLaunches } = require("../../models/launches.model");
+const { getAllLaunches, addNewLaunch } = require("../../models/launches.model");
 
 exports.httpGetAllLaunches = (req, res) =>{
     return res.status(200).json(getAllLaunches());
 }
+
+exports.httpPostAddNewLaunch = (req, res) => {
+  return res.status(200).json(addNewLaunch());
+};
