27109a6fbea07094f0177489f4f6a9d48b75180a
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-11-03 05:50:20 +0200

    Update

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -1,7 +1,11 @@
+const request = require('supertest');
+const app = require('../../app');
+
+
 describe('Test GET / launches', () => {
-    test('It should respond with 200 success', () => {
-        const res = 200;
-        expect(res).toBe(200);
+    test('It should respond with 200 success', async () => {
+        const res = await request(app).get('/launches').expect(200);
+        //expect(res.statusCode).toBe(200); // Assersion statement
     })
 });
 describe('Test Post / launch', () => {
