7917c53e9d98888568352640354299702e92264f
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-11-03 05:37:20 +0200

    Update

---
+++ server/src/routes/launches/launches.test.js
@@ -0,0 +1,17 @@
+describe('Test GET / launches', () => {
+    test('It should respond with 200 success', () => {
+        const res = 200;
+        expect(res).toBe(200);
+    })
+});
+describe('Test Post / launch', () => {
+    test("It should respond with 200 success", () => {
+        
+    });
+    test("It should catch missing required properties", () => {
+
+    });
+    test("It should catch invalid dates", () => {
+
+    });
+});
